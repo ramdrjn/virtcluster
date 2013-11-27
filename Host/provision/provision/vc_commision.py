0a9817c9eae82265bcff7e51b21ec10c93c8c954
ramdrjn <ramdrjn@gmail.com>
2013-11-27 20:47:24 +0530

    Commisionig changes

--- Host/provision/provision/vc_commision.py
+++ Host/provision/provision/vc_commision.py
@@ -4,16 +4,33 @@
 from common import common
 import inspect
 
-def _prep_commision(comi_dir):
-    print comi_dir
+def _prep_commision(fname):
+    with open(fname, "w") as f:
+        f.write("Initiated")
 
 def _check_commision_prep(comi_dir):
-    
-    _prep_commision(comi_dir)
+    dat_dir = os.path.join(comi_dir, "dat")
+    try:
+        os.mkdir(dat_dir)
+    except OSError:
+        return (-1, "dat directory not found")
+
+    fname = os.path.join(dat_dir, "commision.dat")
+
+    if not os.path.isfile(fname):
+        _prep_commision(fname)
+
+    with open(fname) as f:
+        state=f.readline()
+
+    if state != "Initiated" or state != "Done":
+        return (-1, "commision.dat state not valid")
+
+    return (0, "success")
 
 def start(comi_dir, arg_d):
     common.log(common.debug,
                "In Function {0}".format(inspect.stack()[0][3]))
-
-    _check_commision_prep(comi_dir)
+    ck_status = _check_commision_prep(comi_dir)
     print arg_d
+    print ck_status
