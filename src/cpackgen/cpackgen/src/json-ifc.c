763a2ebcd93e799ef452f6024c0327ac18ee38b4
ramdrjn <ramdrjn@gmail.com>
2013-12-26 16:55:15 +0530

    c json interface

---
+++ src/cpackgen/cpackgen/src/json-ifc.c
@@ -0,0 +1,8 @@
+/**
+ * @file json-ifc.c
+ * Contains json decode and encode funtions.
+ * @author RD
+ * @date Thu Dec 26 16:54:29 IST 2013
+ */
+
+/****************************************************************************/
