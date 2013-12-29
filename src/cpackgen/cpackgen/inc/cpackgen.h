e586d709b6244b5d94c83b80b49e981d719e00a6
ramdrjn <ramdrjn@gmail.com>
2013-12-29 10:56:16 +0530

    Log only in files

--- src/cpackgen/cpackgen/inc/cpackgen.h
+++ src/cpackgen/cpackgen/inc/cpackgen.h
@@ -26,9 +26,9 @@ typedef enum
     NIL
   } cmd_e_type;
 
-#define debug(fmt, ...) bs_log(lObj, LVL_DEBUG, PF_LOG, "\n"fmt, __VA_ARGS__)
-#define info(fmt, ...) bs_log(lObj, LVL_INFO, PF_LOG, "\n"fmt, __VA_ARGS__)
-#define error(fmt, ...) bs_log(lObj, LVL_ERROR, PF_LOG, "\n"fmt, __VA_ARGS__)
+#define debug(fmt, ...) bs_log(lObj, LVL_DEBUG, FO_LOG, "\n"fmt, __VA_ARGS__)
+#define info(fmt, ...) bs_log(lObj, LVL_INFO, FO_LOG, "\n"fmt, __VA_ARGS__)
+#define error(fmt, ...) bs_log(lObj, LVL_ERROR, FO_LOG, "\n"fmt, __VA_ARGS__)
 
 genErr_t parse_json(const char *input, void **jobj_ref, bs_lmodCls lObj);
 void free_json_obj(void *jobj_ref);
